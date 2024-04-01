61673e6b5c78126a405575801eca710a0b7e2220
Gautier <benaim.gautier@gmail.com>
2024-04-02 00:24:14 +0200

    feat: added rationale on login page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "modern-normalize/modern-normalize.css";
+</script>
+
+<svelte:head>
+  <title>Timeline</title>
+</svelte:head>
+
+<slot />
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,1 +1,31 @@
-<a href="/auth/google" data-sveltekit-reload>Sign in with google</a>
+<main>
+  <h1>Timeline</h1>
+  <p>The social calendar</p>
+  <p>
+    <a href="/auth/google" data-sveltekit-reload>Sign in with google</a>
+  </p>
+  <h2>What is Timeline?</h2>
+  <p>
+    Timeline allows people to share what they did and what they plan to do,
+    using
+    <em>Events</em>. Share your holiday photos with your family, plan vacations
+    with your friends, follow your favorite band's tour, or just share your
+    daily life with your followers. It's up to you!
+  </p>
+  <h2>Why?</h2>
+  <p>
+    Sharing events with people happens all the time, yet there is still no
+    enjoyable way to do it. Either you share a cloud calendar with them, and you
+    need one per group of people you share things with, or you invite them by
+    email for all events, which is a terribly cumbersome.
+  </p>
+  <p>Timeline is the missing social calendar.</p>
+</main>
+
+<style lang="scss">
+  main {
+    max-width: 60rem;
+    padding-inline: 1rem;
+    margin-inline: auto;
+  }
+</style>
