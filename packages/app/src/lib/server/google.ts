368b2e5152fee65ddf37f86e1aaea38759b15b51
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-01-09 00:31:01 +0100

    fix: compute google redirect uri on the fly

--- packages/app/src/lib/server/google.ts
+++ packages/app/src/lib/server/google.ts
@@ -4,5 +4,4 @@ import { google } from "googleapis";
 export const client = new google.auth.OAuth2(
   env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:5173/auth/callback-google",
 );
--- packages/app/src/routes/auth/+page.server.ts
+++ packages/app/src/routes/auth/+page.server.ts
@@ -1,7 +1,9 @@
 import { client } from "$lib/server/google.js";
 
-export const load = () => {
+export const load = ({ request }) => {
   const url = client.generateAuthUrl({
+    redirect_uri: new URL("/auth/callback-google", request.url).toString(),
+
     // offline means "produce tokens that can be used without user interaction"
     access_type: "offline",
     scope: [
