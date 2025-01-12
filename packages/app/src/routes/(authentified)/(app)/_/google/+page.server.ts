7d82c6aef8bbec6afd6cb86c537ff6dbfcef3ae3
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-01-13 00:41:10 +0100

    list events from google

---
+++ packages/app/src/routes/(authentified)/(app)/_/google/+page.server.ts
@@ -0,0 +1,39 @@
+import { prisma } from "$lib/server/prisma.js";
+import type { Prisma } from "@prisma/client";
+import { google } from "googleapis";
+
+export const load = async ({ parent, url }) => {
+  const { session } = await parent();
+  const auth = new google.auth.OAuth2({ credentials: session.tokens as never });
+
+  auth.on("tokens", async (tokens) => {
+    console.log("Tokens updated");
+    await prisma.googleUser.update({
+      where: { id: session.id },
+      data: { tokens: tokens as Prisma.InputJsonObject },
+    });
+  });
+
+  const calendar = google.calendar({ version: "v3", auth });
+
+  const { data: calendars } = await calendar.calendarList.list();
+
+  const id = url.searchParams.get("id");
+
+  if (id) {
+    const { data: events } = await calendar.events.list({
+      calendarId: id,
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+    });
+    return {
+      calendars: calendars.items ?? [],
+      events: events.items ?? [],
+    };
+  }
+
+  return {
+    calendars: calendars.items ?? [],
+  };
+};
---
+++ supabase/migrations/20250112121723_removed_view.sql
@@ -0,0 +1,1 @@
+DROP VIEW "auth_users";
