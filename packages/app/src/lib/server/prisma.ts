04c7239ddab1352fb20ca79d39c0fe5b6bebf784
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-07-20 15:56:29 +0200

    use miniflare during development

--- packages/app/src/lib/server/prisma.ts
+++ packages/app/src/lib/server/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "db";
+import type { PrismaClient } from "db";
 import { getRequestEvent } from "$app/server";
 
 export const prisma = new Proxy({} as PrismaClient, {
--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -1,17 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
+    cloudflare(),
     sveltekit(),
     icons({ compiler: "svelte", scale: 1.25, defaultClass: "icon" }),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: { api: "modern" },
-    },
-  },
   ssr: {
     noExternal: ["formgator"],
     external: ["db"],
