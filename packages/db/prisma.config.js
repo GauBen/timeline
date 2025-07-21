1f6ec21a6cdb4bb84732501c4e1b2ac7e2719882
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-07-21 22:23:25 +0200

    prisma studio works

--- packages/db/prisma.config.js
+++ packages/db/prisma.config.js
@@ -1,20 +1,22 @@
-/// @ts-check
 import { getPlatformProxy } from "wrangler";
 import { PrismaD1 } from "@prisma/adapter-d1";
+import path from "node:path";
+
+const platform = await getPlatformProxy({
+  persist: {
+    path: path.join(process.env.PROJECT_CWD, ".wrangler/state/v3"),
+  },
+});
+const d1 = new PrismaD1(platform.env.DB);
 
 /** @type {import("prisma").PrismaConfig} */
 export default {
   earlyAccess: true,
-  async adapter() {
-    const platform = await getPlatformProxy({
-      configPath: "../app/wrangler.json",
-      persist: { path: "../app/.wrangler/state/v3" },
-    });
-    const d1 = new PrismaD1(platform.env.DB);
-    const shadowD1 = new PrismaD1(platform.env.SHADOW_DB);
-    return {
-      ...d1,
-      connectToShadowDb: shadowD1.connect,
-    };
+
+  adapter: () => d1,
+
+  studio: {
+    // The studio requires its own adapter for now
+    adapter: () => d1,
   },
 };
