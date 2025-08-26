04919151e68fd4ce7e584f240d5339c15e1accb6
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-08-27 00:06:34 +0200

    chore: bump devalue and remove custom hooks

--- packages/app/src/hooks.ts
+++ packages/app/src/hooks.ts
@@ -1,15 +1,1 @@
-import type { Transport, Transporter } from "@sveltejs/kit";
 import "temporal-polyfill/global";
-
-export const transport: Transport = {
-  PlainDate: {
-    encode: (value) =>
-      value instanceof Temporal.PlainDate && [value.toString()],
-    decode: ([value]) => Temporal.PlainDate.from(value),
-  } satisfies Transporter<Temporal.PlainDate, [string]>,
-  PlainDateTime: {
-    encode: (value) =>
-      value instanceof Temporal.PlainDateTime && [value.toString()],
-    decode: ([value]) => Temporal.PlainDateTime.from(value),
-  } satisfies Transporter<Temporal.PlainDateTime, [string]>,
-};
